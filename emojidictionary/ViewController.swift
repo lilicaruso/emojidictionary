b9d4aa06731321c374808ef24ee7d361b4f9e9fa
tokocaruso <tokocaruso@gmail.com>
2017-09-08 19:33:09 -0400

    Initial Commit

---
+++ emojidictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emojidictionary
+//
+//  Created by toko on 9/8/17.
+//  Copyright (c) 2017 toko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
