cfdd630e943a09487f59317c57b6773ac2e09496
tokocaruso <tokocaruso@gmail.com>
2017-09-08 20:05:19 -0400

    display rows

--- emojidictionary/ViewController.swift
+++ emojidictionary/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var lilitableview: UITableView!
+    
+    var emojis = ["😏","🙋","💜"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        lilitableview.delegate = self
+        lilitableview.dataSource = self
+        
     }
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+        
+        }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +40,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
